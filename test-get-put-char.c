71ebe5e745325f1018c8a8aa19a11d376cb37fc6
shuihan <1656312770@qq.com>
2019-07-05 13:46:11 +0800

    change

--- test-get-put-char.c
+++ test-get-put-char.c
@@ -3,8 +3,14 @@
 int main(void)
 {
     char ch;
-    while((ch = getchar()) != '#'){
+    //while((ch = getchar()) != '#'){
+    while((ch = getchar()) != EOF)
+    {
         putchar(ch);
     }
     return 0;
 }
+
+
+//Input: i am shuihan, this is my blog # haha
+//Output: i am shuihan, this is my blog 
